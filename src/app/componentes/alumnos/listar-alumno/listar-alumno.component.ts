dc1f8278c2383be7912707bfb887ccfb80a4cd7a
francocg <francocabanillas@upeu.edu.pe>
2020-10-22 17:46:21 -0500

    completo

---
+++ src/app/componentes/alumnos/listar-alumno/listar-alumno.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Alumno,Escuela } from 'src/app/modelo/alumno';
+import { AlumnoService } from 'src/app/services/alumno.service';
+
+@Component({
+  selector: 'app-listar-alumno',
+  templateUrl: './listar-alumno.component.html',
+  styleUrls: ['./listar-alumno.component.css']
+})
+export class ListarAlumnoComponent implements OnInit {
+  alumnos: Alumno[];
+  constructor(private alumnoSevice: AlumnoService) { }
+
+  ngOnInit(): void {
+    this.alumnos = this.alumnoSevice.getAlumnos();
+  }
+
+}
